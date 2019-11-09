08e71173418387cef4edf9fbc1e9d2481514b309
wangke <49186456@qq.com>
2019-11-09 13:48:53 +0800

    修改 UserDO 属性 userId 为 id

--- demo-springcloud-2-provider/src/main/java/com/alisls/demo/springcloud/provider/entity/UserDO.java
+++ demo-springcloud-2-provider/src/main/java/com/alisls/demo/springcloud/provider/entity/UserDO.java
@@ -1,29 +1,17 @@
 package com.alisls.demo.springcloud.provider.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity(name = "my_user")
+import lombok.Data;
+
+@Entity(name = "sys_user")
+@Data
 public class UserDO {
 
 	@Id
 	//@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name = "user_id")
-	private Integer userId;
+	private Integer id;
 	private String username;
-	
-	public Integer getUserId() {
-		return userId;
-	}
-	public void setUserId(Integer userId) {
-		this.userId = userId;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
 	
 }
--- demo-springcloud-2-provider/src/main/java/com/alisls/demo/springcloud/provider/repository/UserRepository.java
+++ demo-springcloud-2-provider/src/main/java/com/alisls/demo/springcloud/provider/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.alisls.demo.springcloud.provider.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alisls.demo.springcloud.provider.entity.UserDO;
 
-public interface UserRepository extends CrudRepository<UserDO, Integer> {
-
-	public UserDO findByUserId(Integer userId);
+public interface UserRepository extends JpaRepository<UserDO, Integer> {
 	
 }
