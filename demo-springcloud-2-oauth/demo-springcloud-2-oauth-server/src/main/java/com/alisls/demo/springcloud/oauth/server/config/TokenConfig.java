1da2fb1361281e267eae3db0afe95d9aa7a08c66
wangke <49186456@qq.com>
2020-03-17 09:12:47 +0800

    增加类JWT Token的非对称加密

--- demo-springcloud-2-oauth/demo-springcloud-2-oauth-server/src/main/java/com/alisls/demo/springcloud/oauth/server/config/TokenConfig.java
+++ demo-springcloud-2-oauth/demo-springcloud-2-oauth-server/src/main/java/com/alisls/demo/springcloud/oauth/server/config/TokenConfig.java
@@ -1,15 +1,18 @@
 package com.alisls.demo.springcloud.oauth.server.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.sql.visitor.functions.Char;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import javax.sql.DataSource;
@@ -38,7 +41,23 @@ public class TokenConfig {
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
         // 采用对称加密签名令牌，对应资源服务器也要采用此密钥进行解密
-        jwtAccessTokenConverter.setSigningKey(SIGNING_KEY);
+        //jwtAccessTokenConverter.setSigningKey(SIGNING_KEY);
+
+        /*
+         * 采用非对称加密加密jwt
+         *
+         */
+        // 密钥库资源
+        ClassPathResource keyStoreResource = new ClassPathResource("oauth2.jks");
+        // 密钥库口口令
+        String storePass = "oauth2";
+        // 使用密钥库和密钥库口令，用私钥进行签名
+        KeyStoreKeyFactory keyStoreKeyFactory =
+                new KeyStoreKeyFactory(keyStoreResource, storePass.toCharArray());
+
+        // 密钥库别名
+        String alias = "oauth2";
+        jwtAccessTokenConverter.setKeyPair(keyStoreKeyFactory.getKeyPair(alias));
         return jwtAccessTokenConverter;
     }
 
--- demo-springcloud-2-service-product/demo-springcloud-2-service-product-service/src/main/java/com/alisls/demo/springcloud/service/product/config/TokenConfig.java
+++ demo-springcloud-2-service-product/demo-springcloud-2-service-product-service/src/main/java/com/alisls/demo/springcloud/service/product/config/TokenConfig.java
@@ -1,10 +1,15 @@
 package com.alisls.demo.springcloud.service.product.config;
 
+import io.micrometer.core.instrument.util.IOUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 /**
  * Token配置类
@@ -27,7 +32,20 @@ public class TokenConfig {
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
         // 采用对称加密签名令牌，对应资源服务器也要采用此密钥进行解密
-        jwtAccessTokenConverter.setSigningKey(SIGNING_KEY);
+        //jwtAccessTokenConverter.setSigningKey(SIGNING_KEY);
+
+        /*
+         * 采用非对称加密解密 JWT
+         */
+        ClassPathResource classPathResource = new ClassPathResource("publicKey.txt");
+        String publicKey = null;
+        try {
+            publicKey = IOUtils.toString(classPathResource.getInputStream(), Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            System.out.println("读取到公钥：" + publicKey);
+        }
+
+        jwtAccessTokenConverter.setVerifierKey(publicKey);
         return jwtAccessTokenConverter;
     }
 
