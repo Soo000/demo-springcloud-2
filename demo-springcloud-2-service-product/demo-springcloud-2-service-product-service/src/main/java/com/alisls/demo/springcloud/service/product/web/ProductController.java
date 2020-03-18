5a68a2db9ccc817db0cc0f3fade95d25da6496c2
wangke <49186456@qq.com>
2020-03-19 00:15:32 +0800

    网关demo-springcloud-2-zuul-oauth实现权限认证拦截和转发

--- demo-springcloud-2-service-product/demo-springcloud-2-service-product-service/src/main/java/com/alisls/demo/springcloud/service/product/web/ProductController.java
+++ demo-springcloud-2-service-product/demo-springcloud-2-service-product-service/src/main/java/com/alisls/demo/springcloud/service/product/web/ProductController.java
@@ -1,14 +1,12 @@
 package com.alisls.demo.springcloud.service.product.web;
 
+import com.alisls.demo.springcloud.service.product.dto.ProductDTO;
 import com.springcloud.common.model.dto.DataResult;
 import com.springcloud.common.model.dto.Result;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 商品管理
@@ -26,10 +24,10 @@ public class ProductController {
     @GetMapping("/listProducts")
     //@PreAuthorize("hasAuthority('product:listProducts')")
     public ResponseEntity<Result> listProducts() {
-        List<String> list = new ArrayList<>();
-        list.add("眼睛");
-        list.add("双肩包");
-        return ResponseEntity.ok(DataResult.ofSuccess(list));
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(1000L);
+        productDTO.setProductName("WK的商品");
+        return ResponseEntity.ok(DataResult.ofSuccess(productDTO));
     }
 
 }
